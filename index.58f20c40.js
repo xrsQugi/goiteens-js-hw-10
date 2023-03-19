f5a9c70d967695c43e73944e6c7e2287e21d425d
xrsQugi <110053874+xrsQugi@users.noreply.github.com>
2023-03-19 19:04:19 +0000

    Deploying to gh-pages from @ xrsQugi/goiteens-js-hw-10@c4b5cf77f56b4905733d9d5d82399455f4fb0a63 🚀

---
+++ index.58f20c40.js
@@ -0,0 +1,6 @@
+const e=document.querySelector("#value");let t=0;const n=document.querySelector('[data-action="decrement"]'),o=document.querySelector('[data-action="increment"]'),c=document.querySelector("#name-input"),r=document.querySelector("#name-output"),d=document.querySelector(".color"),u=document.querySelector(".change-color");document.querySelector(".widget");const l=()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;d.textContent=e,document.body.style.backgroundColor=e},a=document.querySelector("#pill3");
+//!=======================homeWork 8============================
+o.addEventListener("click",(()=>{t+=1,e.textContent=t})),n.addEventListener("click",(()=>{t-=1,e.textContent=t})),c.addEventListener("input",(e=>{r.textContent=e.currentTarget.value,""===r.textContent.trim()&&(r.textContent="Anonymous")})),
+//!=======================homeWork 9============================
+a.addEventListener("click",(()=>{a.checked?u.addEventListener("click",l):u.removeEventListener("click",l)}));
+//# sourceMappingURL=index.58f20c40.js.map
